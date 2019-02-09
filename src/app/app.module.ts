181476b20f846d5753e437f82f60d772341fd5e4
Brian Burns <bburns.km@gmail.com>
2019-02-09 07:54:27 -0600

    app - making subcomponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about.component';
 
 import { AgmCoreModule } from '@agm/core';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
