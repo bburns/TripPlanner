3b8916cdd99108553e7e33d77cecef364df0f037
Brian Burns <bburns.km@gmail.com>
2019-02-09 04:23:51 -0600

    map - show google map via agm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'arttour-angular';
+  title: string = 'arttour-angular';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from '@agm/core';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyD2HsJrbaxx_-0uOOq-mPU5ZQCoCio07Yo'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
