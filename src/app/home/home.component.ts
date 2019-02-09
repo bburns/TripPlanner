2b1d33814b3e9e42b65c27ba90d7619554c0de84
Brian Burns <bburns.km@gmail.com>
2019-02-09 08:40:34 -0600

    itenerary - crude start

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  @Input() lat: string;
+  @Input() lng: string;
 
   constructor() { }
 
