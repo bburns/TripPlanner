647db2e90c7995fbe5c091257c26b793aa881ae2
Brian Burns <bburns.km@gmail.com>
2019-02-10 14:11:49 -0600

    header - alert on enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-// import { Input, EventEmitter } from '@angular/core';
-import { ViewChild, ElementRef, NgZone, } from '@angular/core';
-import { MapsAPILoader } from '@agm/core';
-import { GooglePlaceDirective } from 'ngx-google-places-autocomplete/ngx-google-places-autocomplete.directive';
+// import { MapsAPILoader } from '@agm/core';
+// import { ViewChild, ElementRef, NgZone } from '@angular/core';
 
 
 @Component({
@@ -13,27 +11,12 @@ import { GooglePlaceDirective } from 'ngx-google-places-autocomplete/ngx-google-
 export class AppComponent {
 
   title: string = 'TripPlanner';
-  @ViewChild('places') places: GooglePlaceDirective;
-  @ViewChild('search') public searchElement: ElementRef;
-
-  // @Input() setCurrentPlace: EventEmitter<any>;
 
   // default to austin tx
   center: { lat: number, lng: number } = {
     lat: 30.267153,
     lng: -97.7430608,
   }
-
-  currentPlace: any;
-
-  // // spacex launch complex 40, cape canaveral FL
-  // currentPlace: any = {
-  //   lat: 28.562106,
-  //   lng: -80.57718,
-  // };
-
-  // list of colors for lines between places
-  colors: Array<string> = ['red', 'orange', 'yellow', 'green', 'blue', 'violet'];
 
   // list of places in itinerary
   itinerary: Array<object> = [
@@ -42,42 +25,33 @@ export class AppComponent {
     { name: 'SpaceX Launch Complex 40, Cape Canaveral, FL', lat: 28.562106, lng: -80.57718 },
   ];
 
-  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) {
-    //. make a nicer confirm dialog somehow
-    // if (navigator) {
-    //   navigator.geolocation.getCurrentPosition(pos => {
-    //     this.center = {
-    //       lat: +pos.coords.latitude,
-    //       lng: +pos.coords.longitude,
-    //     }
-    //   });
-    // }
-  }
+  currentPlace: any;
 
-  // public handleAddressChange(address: Address) {
-  public handleAddressChange(address) {
-    console.log(address.geometry.location.lng());
-    console.log(address.geometry.location.lat());
-    console.log(address.geometry.location.toJSON());
-    console.log(address.geometry.viewport.getNorthEast());
-    this.center.lat = address.geometry.location.lat();
-    this.center.lng = address.geometry.location.lng();
-  }
+  // list of colors for lines between places
+  colors: Array<string> = ['red', 'orange', 'yellow', 'green', 'blue', 'violet'];
+
+  // constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) {
+  //   //. make a nicer confirm dialog somehow
+  //   // if (navigator) {
+  //   //   navigator.geolocation.getCurrentPosition(pos => {
+  //   //     this.center = {
+  //   //       lat: +pos.coords.latitude,
+  //   //       lng: +pos.coords.longitude,
+  //   //     }
+  //   //   });
+  //   // }
+  // }
 
   public handleSetCurrentPlace(coords) {
-    // alert('click set current place ' + JSON.stringify(coords));
     this.currentPlace = coords;
   }
 
   public handleAddPlace(place) {
-    // alert('add place ' + JSON.stringify(place));
     this.itinerary.push(place);
   }
 
   public handleRemovePlace(i) {
-    // alert('remove place ' + JSON.stringify(i));
     this.itinerary.splice(i, 1);
-    console.log(this.itinerary);
   }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ViewChild, ElementRef, NgZone, } from '@angular/core';
+import { GooglePlaceDirective } from 'ngx-google-places-autocomplete/ngx-google-places-autocomplete.directive';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +10,22 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   @Input() title: string;
+  @ViewChild('places') places: GooglePlaceDirective;
+  @ViewChild('search') public searchElement: ElementRef;
 
   constructor() {
   }
 
-  handleAddressChangeEvent() {
-    
+  _enterAddress(address) {
+    alert("place entered " + JSON.stringify(address));
+    console.log(address.geometry.location.lng());
+    console.log(address.geometry.location.lat());
+    console.log(address.geometry.location.toJSON());
+    console.log(address.geometry.viewport.getNorthEast());
+    // this.center.lat = address.geometry.location.lat();
+    // this.center.lng = address.geometry.location.lng();
   }
-
+  
   ngOnInit() {
   }
 
